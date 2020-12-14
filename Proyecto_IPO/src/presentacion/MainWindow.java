9257e8beba93bdfec2f4d506a1fc4e89df8c68e9
Carlos-Bernalte <carlos.bernalte@outlook.com>
2020-12-14 23:29:46 +0100

    Cambio de diseño en la interfaz principal

--- Proyecto_IPO/src/presentacion/MainWindow.java
+++ Proyecto_IPO/src/presentacion/MainWindow.java
@@ -6,214 +6,135 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
 import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.border.MatteBorder;
-import java.awt.Color;
-import java.awt.Font;
-import javax.swing.JSeparator;
-import java.awt.Component;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.ImageIcon;
-import java.awt.Dimension;
-import javax.swing.border.LineBorder;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
-import javax.swing.border.EtchedBorder;
-import java.awt.ComponentOrientation;
-import javax.swing.JMenu;
-import javax.swing.JPopupMenu;
+import javax.swing.JMenuItem;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.SwingConstants;
+import javax.swing.JPopupMenu;
+import java.awt.Insets;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Color;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
-	private JMenuBar menuBar;
-	private JButton btnConsultarInformacion;
-	private JButton btnRealizarReserva;
-	private JButton btnPersonal;
-	private JButton btnDibujarRuta;
-	private JMenu mnIdioma;
-	private JPanel panelUsuario;
-	private JMenuBar menuUsuario;
-	private JMenu mnNewMenu;
-	private JMenuItem mntmCerrarSesion;
-	private JPanel panelVariable;
+	private JPanel panelCentral;
 	private JLabel lblFeedback;
-	private JPanel panel;
+	private JMenuBar barraOpciones;
+	private JMenu mnIdioma;
+	private JPanel panelSuperior;
+	private JMenuBar MenuOpciones;
+	private JMenu mnConsultarInformacion;
+	private JMenu mnRealizarReserva;
+	private JMenu mnPersonal;
+	private JMenu mnDibujarRuta;
+	private JPanel bannerUsuario;
+	private JLabel banner;
+	private JMenuBar barraUsuario;
+	private JMenu mnUsuario;
+	private JMenuItem mntmCerrarSesion;
+	private JMenuItem mntmEspañol;
+	private JMenuItem mntmIngles;
 
+	/**
+	 * Create the frame.
+	 */
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1176, 742);
+		setBounds(100, 100, 789, 494);
 		
-		menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		barraOpciones = new JMenuBar();
+		setJMenuBar(barraOpciones);
 		
 		mnIdioma = new JMenu("Idioma");
-		menuBar.add(mnIdioma);
+		barraOpciones.add(mnIdioma);
+		
+		mntmEspañol = new JMenuItem("Español");
+		mnIdioma.add(mntmEspañol);
+		
+		mntmIngles = new JMenuItem("Ingles");
+		mnIdioma.add(mntmIngles);
 		contentPane = new JPanel();
-		contentPane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 13, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		panelUsuario = new JPanel();
-		panelUsuario.setBackground(Color.WHITE);
-		GridBagConstraints gbc_panelUsuario = new GridBagConstraints();
-		gbc_panelUsuario.insets = new Insets(0, 0, 5, 0);
-		gbc_panelUsuario.fill = GridBagConstraints.BOTH;
-		gbc_panelUsuario.gridx = 3;
-		gbc_panelUsuario.gridy = 0;
-		contentPane.add(panelUsuario, gbc_panelUsuario);
-		
-		menuUsuario = new JMenuBar();
-		panelUsuario.add(menuUsuario);
-		
-		mnNewMenu = new JMenu("");
-		mnNewMenu.setIcon(new ImageIcon(MainWindow.class.getResource("/imagenes/usuario.png")));
-		menuUsuario.add(mnNewMenu);
-		
-		mntmCerrarSesion = new JMenuItem("Cerrar Sesion");
-		mntmCerrarSesion.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-		mnNewMenu.add(mntmCerrarSesion);
-		
-		btnConsultarInformacion = new JButton("Consultar Información");
-		btnConsultarInformacion.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				btnConsultarInformacion.setBorder(new MatteBorder(2, 2, 0, 2, (Color) new Color(0, 0, 0)));
-				btnRealizarReserva.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnPersonal.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnDibujarRuta.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-			}
-		});
-		btnConsultarInformacion.setBackground(Color.WHITE);
-		btnConsultarInformacion.setBorder(new MatteBorder(2, 2, 0, 2, (Color) new Color(0, 0, 0)));
-		btnConsultarInformacion.setMargin(new Insets(10, 30, 0, 30));
-		GridBagConstraints gbc_btnConsultarInformacion = new GridBagConstraints();
-		gbc_btnConsultarInformacion.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnConsultarInformacion.insets = new Insets(0, 0, 5, 5);
-		gbc_btnConsultarInformacion.gridx = 0;
-		gbc_btnConsultarInformacion.gridy = 1;
-		contentPane.add(btnConsultarInformacion, gbc_btnConsultarInformacion);
-		
-		btnRealizarReserva = new JButton("Realizar Reserva");
-		btnRealizarReserva.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				btnConsultarInformacion.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnRealizarReserva.setBorder(new MatteBorder(2, 2, 0, 2, (Color) new Color(0, 0, 0)));
-				btnPersonal.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnDibujarRuta.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-			}
-		});
-		btnRealizarReserva.setBackground(Color.WHITE);
-		btnRealizarReserva.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		btnRealizarReserva.setMargin(new Insets(10, 30, 0, 30));
-		GridBagConstraints gbc_btnRealizarReserva = new GridBagConstraints();
-		gbc_btnRealizarReserva.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnRealizarReserva.insets = new Insets(0, 0, 5, 5);
-		gbc_btnRealizarReserva.gridx = 1;
-		gbc_btnRealizarReserva.gridy = 1;
-		contentPane.add(btnRealizarReserva, gbc_btnRealizarReserva);
-		
-		btnPersonal = new JButton("Personal");
-		btnPersonal.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				btnConsultarInformacion.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnRealizarReserva.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnPersonal.setBorder(new MatteBorder(2, 2, 0, 2, (Color) new Color(0, 0, 0)));
-				btnDibujarRuta.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-			}
-		});
-		btnPersonal.setBackground(Color.WHITE);
-		btnPersonal.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		btnPersonal.setMargin(new Insets(10, 30, 0, 30));
-		GridBagConstraints gbc_btnPersonal = new GridBagConstraints();
-		gbc_btnPersonal.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnPersonal.insets = new Insets(0, 0, 5, 5);
-		gbc_btnPersonal.gridx = 2;
-		gbc_btnPersonal.gridy = 1;
-		contentPane.add(btnPersonal, gbc_btnPersonal);
-		
-		btnDibujarRuta = new JButton("Dibujar Ruta");
-		btnDibujarRuta.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				btnConsultarInformacion.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnRealizarReserva.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnPersonal.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-				btnDibujarRuta.setBorder(new MatteBorder(2, 2, 0, 2, (Color) new Color(0, 0, 0)));
-			}
-		});
-		btnDibujarRuta.setBackground(Color.WHITE);
-		btnDibujarRuta.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		btnDibujarRuta.setMargin(new Insets(10, 30, 0, 30));
-		GridBagConstraints gbc_btnDibujarRuta = new GridBagConstraints();
-		gbc_btnDibujarRuta.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnDibujarRuta.insets = new Insets(0, 0, 5, 0);
-		gbc_btnDibujarRuta.gridx = 3;
-		gbc_btnDibujarRuta.gridy = 1;
-		contentPane.add(btnDibujarRuta, gbc_btnDibujarRuta);
-		
-		panelVariable = new JPanel();
-		panelVariable.setBackground(Color.WHITE);
-		GridBagConstraints gbc_panelVariable = new GridBagConstraints();
-		gbc_panelVariable.gridwidth = 4;
-		gbc_panelVariable.insets = new Insets(0, 0, 5, 0);
-		gbc_panelVariable.fill = GridBagConstraints.BOTH;
-		gbc_panelVariable.gridx = 0;
-		gbc_panelVariable.gridy = 2;
-		contentPane.add(panelVariable, gbc_panelVariable);
-		panelVariable.setLayout(new BorderLayout(0, 0));
-		
-		panel = new JPanel();
-		panelVariable.add(panel, BorderLayout.NORTH);
+		panelCentral = new JPanel();
+		contentPane.add(panelCentral, BorderLayout.CENTER);
 		
 		lblFeedback = new JLabel("Bienvenido");
-		lblFeedback.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_lblFeedback = new GridBagConstraints();
-		gbc_lblFeedback.anchor = GridBagConstraints.WEST;
-		gbc_lblFeedback.gridwidth = 4;
-		gbc_lblFeedback.insets = new Insets(0, 0, 0, 5);
-		gbc_lblFeedback.gridx = 0;
-		gbc_lblFeedback.gridy = 3;
-		contentPane.add(lblFeedback, gbc_lblFeedback);
+		contentPane.add(lblFeedback, BorderLayout.SOUTH);
+		
+		panelSuperior = new JPanel();
+		contentPane.add(panelSuperior, BorderLayout.NORTH);
+		panelSuperior.setLayout(new BorderLayout(0, 0));
+		
+		MenuOpciones = new JMenuBar();
+		panelSuperior.add(MenuOpciones, BorderLayout.CENTER);
+		
+		mnConsultarInformacion = new JMenu("Consultar Información");
+		mnConsultarInformacion.setHideActionText(true);
+		mnConsultarInformacion.setSelected(true);
+		MenuOpciones.add(mnConsultarInformacion);
+		
+		mnRealizarReserva = new JMenu("Realizar Reserva");
+		mnRealizarReserva.setHideActionText(true);
+		MenuOpciones.add(mnRealizarReserva);
+		
+		mnPersonal = new JMenu("Personal");
+		mnPersonal.setHideActionText(true);
+		MenuOpciones.add(mnPersonal);
+		
+		mnDibujarRuta = new JMenu("Dibujar Ruta");
+		mnDibujarRuta.setHideActionText(true);
+		MenuOpciones.add(mnDibujarRuta);
+		
+		bannerUsuario = new JPanel();
+		bannerUsuario.setBackground(Color.DARK_GRAY);
+		panelSuperior.add(bannerUsuario, BorderLayout.NORTH);
+		GridBagLayout gbl_bannerUsuario = new GridBagLayout();
+		gbl_bannerUsuario.columnWidths = new int[]{685, 66, 0};
+		gbl_bannerUsuario.rowHeights = new int[]{89, 0};
+		gbl_bannerUsuario.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_bannerUsuario.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		bannerUsuario.setLayout(gbl_bannerUsuario);
+		
+		banner = new JLabel("BANNER");
+		banner.setBackground(Color.GRAY);
+		banner.setIgnoreRepaint(true);
+		banner.setIconTextGap(7);
+		banner.setSize(new Dimension(3, 3));
+		banner.setAlignmentX(Component.CENTER_ALIGNMENT);
+		banner.setIcon(null);
+		GridBagConstraints gbc_banner = new GridBagConstraints();
+		gbc_banner.fill = GridBagConstraints.HORIZONTAL;
+		gbc_banner.insets = new Insets(0, 0, 0, 5);
+		gbc_banner.gridx = 0;
+		gbc_banner.gridy = 0;
+		bannerUsuario.add(banner, gbc_banner);
+		
+		barraUsuario = new JMenuBar();
+		barraUsuario.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		GridBagConstraints gbc_barraUsuario = new GridBagConstraints();
+		gbc_barraUsuario.fill = GridBagConstraints.BOTH;
+		gbc_barraUsuario.gridx = 1;
+		gbc_barraUsuario.gridy = 0;
+		bannerUsuario.add(barraUsuario, gbc_barraUsuario);
+		
+		mnUsuario = new JMenu("");
+		mnUsuario.setIcon(new ImageIcon(MainWindow.class.getResource("/imagenes/usuario.png")));
+		barraUsuario.add(mnUsuario);
+		
+		mntmCerrarSesion = new JMenuItem("Cerrar sesión");
+		mnUsuario.add(mntmCerrarSesion);
 	}
 
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-	}
 }
